42740b2e338070e155c38fe3f231122ad9a17fdc
Umut Karakulak <umutkarakulak@hotmail.com>
2021-09-20 05:13:46 +0900

    add layouts folder to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  mode: "jit",
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./layouts/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: "class",
   theme: {
     extend: {
